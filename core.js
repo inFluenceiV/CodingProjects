091fc12b1004b964d6e2d8bb37fe1c3120a05474
Nicky Laczko <nicky.laczko@gmail.com>
2014-07-24 22:47:41 -0400

    Update core.js

--- core.js
+++ core.js
@@ -8,9 +8,9 @@ var projects = function(work, progress){
           
           // config = require("./config.js");
           
-          },
+          }
         }
-    }
+    },
   return projects;
   work = this.iV;
   progress++
